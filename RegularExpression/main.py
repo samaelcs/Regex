1cff8199842656b8ed78e6f84af8aa6e88cccb4d
samaelcs <58177112+samaelcs@users.noreply.github.com>
2021-02-10 21:59:08 +0530

    Update main.py

--- RegularExpression/main.py
+++ RegularExpression/main.py
@@ -1,10 +1,12 @@
 from RegularExpressionGen.ParseTree import ParseTree
 from RegularExpressionGen.Evaluate import evaluate
 
-Ptree = ParseTree('( ( A * ) . ( C | B ) )')
+Ptree = ParseTree('( ( A * ) . ( C | B ) )') #Enter regular expression here
 
+#Enter the number of strings you want to generate
+numOut = int(input())
 Outputs = []
-for i in range(10):
+for i in range(numOut):
     while True:
         out = evaluate(Ptree)
         if out not in Outputs:
